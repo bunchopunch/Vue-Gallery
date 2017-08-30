2e449f2ebdbf24af0f24bd5b6e29402eb904353f
bunchopunch <bunchopunch@mustacheforpresident.com>
2017-08-29 20:43:40 -0600

    Refactor computed prop and adjust template

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -4,12 +4,8 @@
     <h2>Albums</h2>
     <div v-for="album in enhancedAlbums">
       <h3 v-text="album.title"></h3>
-      {{album.thubnail}}
       <img v-bind:src="album.thumbnailUrl">
-<!--
-      <img v-for="photo in photos" v-if="photo.albumId === album.id" v-bind:src="photo.thumbnailUrl">
       <button v-on:click="deleteAlbum">Delete Album</button>
-    -->
     </div>
   </div>
 </template>
@@ -36,22 +32,22 @@
             'albumId': 1,
             'id': 1,
             'title': 'Fake Image 1',
-            'url': 'http://placehold.it/600/92c952',
-            'thumbnailUrl': 'http://placehold.it/150/92c952'
+            'url': 'http://placehold.it/600/4b8fbf',
+            'thumbnailUrl': 'http://placehold.it/150/4b8fbf'
           },
           {
             'albumId': 1,
             'id': 2,
             'title': 'Fake Image 2',
-            'url': 'http://placehold.it/600/771796',
-            'thumbnailUrl': 'http://placehold.it/150/771796'
+            'url': 'http://placehold.it/600/32597f',
+            'thumbnailUrl': 'http://placehold.it/150/32597f'
           },
           {
             'albumId': 2,
             'id': 3,
             'title': 'Fake Image 3',
-            'url': 'http://placehold.it/600/771796',
-            'thumbnailUrl': 'http://placehold.it/150/92c952'
+            'url': 'http://placehold.it/600/64b3ff',
+            'thumbnailUrl': 'http://placehold.it/150/64b3ff'
           }
         ]
       }
@@ -66,17 +62,12 @@
     computed: {
       enhancedAlbums () {
         let self = this
-        let a = self.albums.map(function (album) {
-          let albumPhotos = self.photos.filter(function (photo) {
-            if (photo.albumId === album.id) { return true }
-          })
-
-          album.thumbnailUrl = albumPhotos[0].thumbnailUrl
-
+        return self.albums.map(function (album) {
+          album.thumbnailUrl = self.photos.find(function (photo) {
+            return photo.albumId === album.id
+          }).thumbnailUrl
           return album
         })
-        console.log(a)
-        return a
       }
     }
   }
