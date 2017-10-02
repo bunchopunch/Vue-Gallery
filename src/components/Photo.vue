5ff6d52e137bc60382876bdaff55fd8f15dba867
bunchopunch <bunchopunch@mustacheforpresident.com>
2017-10-01 23:53:29 -0600

    Adjust photo template to use getters

--- src/components/Photo.vue
+++ src/components/Photo.vue
@@ -6,54 +6,22 @@
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        albums: [
-          {
-            'userId': 1,
-            'id': 1,
-            'title': 'Fake Album 1'
-          },
-          {
-            'userId': 1,
-            'id': 2,
-            'title': 'Fake Album 2'
-          }
-        ],
-        photos: [
-          {
-            'albumId': 1,
-            'id': 1,
-            'title': 'Fake Image 1',
-            'url': 'http://placehold.it/600/4b8fbf',
-            'thumbnailUrl': 'http://placehold.it/150/4b8fbf'
-          },
-          {
-            'albumId': 1,
-            'id': 2,
-            'title': 'Fake Image 2',
-            'url': 'http://placehold.it/600/32597f',
-            'thumbnailUrl': 'http://placehold.it/150/32597f'
-          },
-          {
-            'albumId': 2,
-            'id': 3,
-            'title': 'Fake Image 3',
-            'url': 'http://placehold.it/600/64b3ff',
-            'thumbnailUrl': 'http://placehold.it/150/64b3ff'
-          }
-        ]
-      }
-    },
+import { mapGetters } from 'vuex'
 
-    methods: {},
-    computed: {
-      currentPhoto () {
-        return this.photos.find((item) => item.id === parseInt(this.$route.params.id))
-      }
+export default {
+  name: 'Photo',
+
+  computed: {
+    ...mapGetters({
+      albums: 'getAlbums',
+      photos: 'getPhotos'
+    }),
+
+    currentPhoto () {
+      return this.photos.find((item) => item.id === parseInt(this.$route.params.id))
     }
   }
+}
 </script>
 
 <style scoped>
