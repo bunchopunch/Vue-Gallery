c85c7d0d99aa9440c847dd5918272b9334cab85c
bunchopunch <bunchopunch@mustacheforpresident.com>
2017-10-10 18:17:26 -0600

    Start working with some Bootstrap elements

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
-    <h2>Albums</h2>
-    <div v-for="album in albums" :key="album.id" class="album-summary">
-      <h3><router-link :to="{ path: 'Album/' + album.id }">{{album.title}}</router-link></h3>
-      <router-link :to="{ path: 'Album/' + album.id }"><img v-bind:src="album.thumbnailUrl"></router-link>
-      <button class="btn btn-danger" v-on:click="deleteAlbum">Delete Album</button>
+    <h2>Newest Albums</h2>
+    <div class="card-columns">
+      <div v-for="album in albums" :key="album.id" class="card album-summary">
+        <img class="card-img-top" v-bind:src="album.thumbnailUrl" alt="Card image cap" />
+        <div class="card-body">
+          <h3 class="card-title"><router-link :to="{ path: 'Album/' + album.id }">{{album.title}}</router-link></h3>
+          <h4 class="card-subtitle mb-2 text-muted">Album ID Number {{album.id}}</h4>
+          <p class="card-text">One of User {{album.userId}}'s albums.</p>
+          <!-- <router-link :to="{ path: 'Album/' + album.id }"><img class="img-thumbnail"></router-link> -->
+          <button class="btn btn-danger" v-on:click="deleteAlbum">Delete Album</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
