f3efb32899e2347cad978e601af3ea90235a7d53
bunchopunch <bunchopunch@mustacheforpresident.com>
2017-10-15 00:00:53 -0600

    Start working up an album style.

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
     <h2>{{albums[$route.params.id+1].title}}</h2>
-    <div v-for="photo in photos" :key="photo.id" v-if="photo.albumId === parseInt($route.params.id)" >
-      <h3 v-text="photo.title"></h3>
-      <p>This is a photo from Album {{photo.albumId}}</p>
-      <router-link :to="{ path: '/Photo/' + photo.id }"><img v-bind:src="photo.thumbnailUrl"></router-link>
+    <div class="album text-muted">
+      <div class="container">
+        <div class="row">
+
+            <div class="card col-6 col-lg-4" v-for="photo in photos" :key="photo.id" v-if="photo.albumId === parseInt($route.params.id)" >
+              <div class="row">
+                <div class="col-3">
+                  <router-link class="float-left" :to="{ path: '/Photo/' + photo.id }"><img class="img-thumbnail" v-bind:src="photo.thumbnailUrl"></router-link>
+                </div>
+                <div class="col-9">
+                  <h3 v-text="photo.title"></h3>
+                  <p>This is a photo from Album {{photo.albumId}}</p>
+                </div>
+              </div>
+            </div>
+
+        </div>
+      </div>
     </div>
+
   </div>
 </template>
 
@@ -13,7 +28,7 @@
   import { mapGetters } from 'vuex'
 
   export default {
-    name: 'Album View',
+    name: 'Album-View',
 
     // TODO: Add a way to get the album title
 
