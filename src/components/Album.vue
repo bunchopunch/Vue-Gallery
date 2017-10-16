42fe2df8d4b6d3018204f1d0495957b5b509a2a3
bunchopunch <bunchopunch@mustacheforpresident.com>
2017-10-16 01:19:02 -0600

    Finish adding media objects for now. Style tweaks.

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="album">
-    <div class="card bg-secondary my-2 px-2">
-      <h2>{{albums[$route.params.id+1].title}}</h2>
+    <div class="my-3 px-2">
+      <h2>Images from {{albums[$route.params.id+1].title}}</h2>
     </div>
     <div class="row">
 
-      <div class="col-4" v-for="photo in photos" :key="photo.id" v-if="photo.albumId === parseInt($route.params.id)" >
-        <div class="media mb-2 p-3">
-          <router-link :to="{ path: '/Photo/' + photo.id }"><img class="img-thumbnail d-flex mr-3" v-bind:src="photo.thumbnailUrl"></router-link>
-          <div class="media-body">
-            <h3 v-text="photo.title"></h3>
-            <p>This is a photo from Album {{photo.albumId}}</p>
+      <div class="col-sm-12 col-md-6 col-lg-4 mb-4" v-for="photo in photos" :key="photo.id" v-if="photo.albumId === parseInt($route.params.id)" >
+        <router-link :to="{ path: '/Photo/' + photo.id }">
+          <div class="media p-3 h-100 border rounded">
+            <img class="img-thumbnail mr-3" v-bind:src="photo.thumbnailUrl">
+            <div class="media-body">
+              <h3 class="h5">{{photo.title}}</h3>
+              A photo from Album {{photo.albumId}}.
+            </div>
           </div>
-        </div>
+        </router-link>
       </div>
 
     </div>
--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,22 +1,24 @@
 <template>
-  <div>
-    <h2>Newest Albums</h2>
-    <div class="row text-center text-lg-left">
+  <div class="gallery">
+    <div class="my-3 px-2">
+      <h2>Newest Albums</h2>
+    </div>
+    <div class="row text-center text-md-left">
 
-        <template v-for="(album, index) in albums">
-          <div class="col-lg-3 col-md-4 col-xs-6 mb-4" :key="album.id">
-            <div class="card album-summary d-block mb-4 h-100">
-              <img class="card-img-top" v-bind:src="album.thumbnailUrl" alt="Card image cap" />
-              <div class="card-body">
-                <h3 class="card-title"><router-link :to="{ path: 'Album/' + album.id }">{{album.title}}</router-link></h3>
-                <h4 class="card-subtitle mb-2 text-muted">Album ID Number {{album.id}}</h4>
-                <p class="card-text">One of User {{album.userId}}'s albums.</p>
-                <!-- <router-link :to="{ path: 'Album/' + album.id }"><img class="img-thumbnail"></router-link> -->
-                <button class="btn btn-danger" v-on:click="deleteAlbum">Delete Album</button>
-              </div>
+      <template v-for="(album, index) in albums">
+        <div class="col-lg-3 col-md-4 col-xs-6 mb-4" :key="album.id">
+          <div class="card album-summary d-block mb-4 h-100">
+            <router-link :to="{ path: 'Album/' + album.id }"><img class="card-img-top" v-bind:src="album.thumbnailUrl" alt="Card image cap" /></router-link>
+            <div class="card-body">
+              <h3 class="card-title h5"><router-link :to="{ path: 'Album/' + album.id }">{{album.title}}</router-link></h3>
+              <h4 class="card-subtitle mb-2 h6 text-muted">Album ID Number {{album.id}}</h4>
+              <p class="card-text">One of User {{album.userId}}'s albums.</p>
+              <!-- <router-link :to="{ path: 'Album/' + album.id }"><img class="img-thumbnail"></router-link> -->
+              <button class="btn btn-danger" v-on:click="deleteAlbum">Delete Album</button>
             </div>
           </div>
-        </template>
+        </div>
+      </template>
 
     </div>
   </div>
