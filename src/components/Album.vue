04c60f5740a274c1caffb99f95003509a0d0a56e
bunchopunch <bunchopunch@mustacheforpresident.com>
2017-10-15 23:56:18 -0600

    tweak styles, change album to use media object

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,26 +1,21 @@
 <template>
-  <div>
-    <h2>{{albums[$route.params.id+1].title}}</h2>
-    <div class="album text-muted">
-      <div class="container">
-        <div class="row">
+  <div class="album">
+    <div class="card bg-secondary my-2 px-2">
+      <h2>{{albums[$route.params.id+1].title}}</h2>
+    </div>
+    <div class="row">
 
-            <div class="card col-6 col-lg-4" v-for="photo in photos" :key="photo.id" v-if="photo.albumId === parseInt($route.params.id)" >
-              <div class="row">
-                <div class="col-3">
-                  <router-link class="float-left" :to="{ path: '/Photo/' + photo.id }"><img class="img-thumbnail" v-bind:src="photo.thumbnailUrl"></router-link>
-                </div>
-                <div class="col-9">
-                  <h3 v-text="photo.title"></h3>
-                  <p>This is a photo from Album {{photo.albumId}}</p>
-                </div>
-              </div>
-            </div>
-
+      <div class="col-4" v-for="photo in photos" :key="photo.id" v-if="photo.albumId === parseInt($route.params.id)" >
+        <div class="media mb-2 p-3">
+          <router-link :to="{ path: '/Photo/' + photo.id }"><img class="img-thumbnail d-flex mr-3" v-bind:src="photo.thumbnailUrl"></router-link>
+          <div class="media-body">
+            <h3 v-text="photo.title"></h3>
+            <p>This is a photo from Album {{photo.albumId}}</p>
+          </div>
         </div>
       </div>
-    </div>
 
+    </div>
   </div>
 </template>
 
