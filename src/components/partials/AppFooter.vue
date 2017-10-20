2d250ec33063b9a9ff24cd5c12ff5986d08f707f
bunchopunch <bunchopunch@mustacheforpresident.com>
2017-10-19 23:59:10 -0600

    Add new component vue files

---
+++ src/components/partials/AppFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="text-center">
+    <p>Just a simple Gallery App.</p>
+  </footer>
+</template>
+
+<script>
+  export default {
+    name: 'AppFooter'
+  }
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/partials/AppMasterhead.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container widget masterhead">
+    <nav class="nav">
+      <h1><router-link class="nav-link active" to='/'>GalleryApp</router-link></h1>
+      <a class="nav-link" href="#">About</a>
+    </nav>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AppMasterhead'
+  }
+</script>
+
+<style scoped>
+.masterhead {
+  background-color: theme-color("panel")
+}
+</style>
