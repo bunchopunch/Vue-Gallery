a70bda8b97a39c7a55d03c44c6ebbe6228435f6b
bunchopunch <bunchopunch@mustacheforpresident.com>
2017-10-31 13:24:11 -0600

    Create the actual pagination widget

---
+++ src/components/partials/WidgetPagination.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="bg-dark pg-dark" aria-label="Page navigation example">
+    <ul class="pagination justify-content-center">
+      <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+      <li class="page-item"><a class="page-link" href="#">1</a></li>
+      <li class="page-item"><a class="page-link" href="#">{{ $route.params.pageNumber }}</a></li>
+      <li class="page-item"><a class="page-link" href="#">3</a></li>
+      <li class="page-item"><a class="page-link" href="#">Next</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'WidgetPagination'
+  }
+</script>
+
+<style scoped>
+</style>
